49eecfe8acdc4620762c3f840c65e1c14df131e0
jjjjung1xx1 <162879975+jjjjung1xx1@users.noreply.github.com>
2024-11-30 23:48:10 +0900

    Update StudentMode.cpp / 과목명 문자열로 입력하도록 수정
    기존 : 출력된 수강 과목 리스트에서 성적을 조회할 과목의 번호를 입력
    수정 : 과목명을 문자열로 입력
    수정 사유 : 과목명을 직접 입력받아 과목명.csv 로의 변환을 용이하게 하기 위함

--- StudentMode.cpp
+++ StudentMode.cpp
@@ -1,39 +1,53 @@
 #include <iostream>
+#include <string>
+#include <vector>
 #include "DisplayStudentCourses.h"
 
 using namespace std;
 
 int main() {
-    string studentName;
 
     /**사용자로부터 학생 이름 입력받기(로그인 과정에서 입력받지 않으나.. 
-    학생목록 csv에서 정보 불러오기 해야 할 것 같기도 합니다)**/
-    cout << "학생 이름을 입력하세요: ";
+    학생목록 csv에서 정보 불러오기 해야 할 것 같기도 합니다 
+    로그인도 했는데 다시 이름 입력하라고 하는건 어색하니까요..!**/
+    string studentName;
+    cout << "학생의 이름을 입력하세요: ";
     cin >> studentName;
 
-    // 파일 경로 설정 및 학생 수강 과목 출력
+    // 학생 수강 과목 출력 및 해당 리스트 저장
     string filename = "StudentCourses.csv";
-    DisplayStudentCourses(filename, studentName);
+    vector<string> courses = DisplayStudentCourses(filename, studentName);
 
     if (courses.empty()) {
-        std::cout << "수강 중인 과목이 없습니다.\n";
+        cout << "수강 중인 과목이 없습니다.\n";
         return 0; // 수강 과목이 없으면 종료
     }
 
+    // 학생이 성적을 조회할 과목 번호 입력
     int chooseCourse;
-    cout << "\n조회할 과목 번호를 입력하세요";
-    cin >> chooseCourse;
+    while (true) {
+        cout << "\n조회할 과목명을 입력하세요.";
+        cin >> chooseCourse;
 
-    // 입력 값 검증
-    if (chooseCourse < 1 || chooseCourse > courses.size()) {
-        cerr << "잘못된 선택입니다. 프로그램을 종료합니다.\n";
-        return 0;
+       // courses 벡터에서 입력한 과목명이 있는지 확인
+        auto it = find(courses.begin(), courses.end(), selectedCourse);
+
+        if (it != courses.end()) {
+            // 과목명을 찾으면 종료
+            break;
+        } else {
+            cout << "잘못된 과목명입니다. 다시 입력하세요.\n";
+        }
     }
+    cout << selectedCourse << "과목의 성적을 조회합니다."
 
-    // 선택한 과목의 이름 가져오기
-    string selectedCourse = courses[chooseCourse - 1];
+    // 선택한 과목의 .csv 파일 찾기
+    string courseFile = FindCourseFile(selectedCourse);
 
-    // 선택한 과목을 문자열로 바꿔서 .csv 로 바꾸고 .csv 목록 중 해당 파일을 찾는 코드 추가 필요
+    if (courseFile.empty()) {
+        cerr << "해당 과목의 .csv 파일을 시스템에서 찾을 수 없습니다.\n";
+        return 0; // 종료
+    }
 
     // 해당 과목의 세부 성적 출력
     DisplayCourseGrades(filename, studentName);
